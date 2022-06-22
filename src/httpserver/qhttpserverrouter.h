2f76d273fae803094ac08869b1c98aa11695e237
Marc Mutz <marc.mutz@qt.io>
2022-06-22 11:08:38 +0200

    QHttpServerRouter:::bindCapturedImpl: use constexpr-if, not enable_if
    Easier to see what's going on.
    
    Task-number: QTBUG-100868
    Change-Id: Iffbefa3acfe80a17304c5668aba3804590c4898f
    Reviewed-by: Jesus Fernandez <jsfdez@gmail.com>
    Reviewed-by: Ievgenii Meshcheriakov <ievgenii.meshcheriakov@qt.io>
    (cherry picked from commit dda07245b3cfdf440a6d54d9b94d001f4aaccb61)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/httpserver/qhttpserverrouter.h
+++ src/httpserver/qhttpserverrouter.h
@@ -100,29 +100,23 @@ private:
                      std::initializer_list<int> metaTypes);
 
     template<typename ViewHandler, typename ViewTraits, int ... Cx, int ... Px>
-    typename std::enable_if<ViewTraits::Arguments::CapturableCount != 0, typename ViewTraits::BindableType>::type
+    typename ViewTraits::BindableType
             bindCapturedImpl(ViewHandler &&handler,
                           const QRegularExpressionMatch &match,
                           QtPrivate::IndexesList<Cx...>,
                           QtPrivate::IndexesList<Px...>) const
     {
-        return std::bind(
+        if constexpr (ViewTraits::Arguments::CapturableCount != 0) {
+            return std::bind(
                 std::forward<ViewHandler>(handler),
                 QVariant(match.captured(Cx + 1))
                     .value<typename ViewTraits::Arguments::template Arg<Cx>::CleanType>()...,
                 QtPrivate::QHttpServerRouterPlaceholder<Px>{}...);
-    }
-
-    template<typename ViewHandler, typename ViewTraits, int ... Cx, int ... Px>
-    typename std::enable_if<!ViewTraits::Arguments::CapturableCount, typename ViewTraits::BindableType>::type
-            bindCapturedImpl(ViewHandler &&handler,
-                          const QRegularExpressionMatch &,
-                          QtPrivate::IndexesList<Cx...>,
-                          QtPrivate::IndexesList<Px...>) const
-    {
-        return std::bind(
+        } else {
+            return std::bind(
                 std::forward<ViewHandler>(handler),
                 QtPrivate::QHttpServerRouterPlaceholder<Px>{}...);
+        }
     }
 
     QScopedPointer<QHttpServerRouterPrivate> d_ptr;
