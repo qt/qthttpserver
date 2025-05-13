c4dd2505bd0c623b29d8e976bb34ddc30c5d827a
Øystein Heskestad <oystein.heskestad@qt.io>
2025-05-13 17:26:48 +0200

    Mark files parsing network traffic as security critical
    In accordance with QUIP 23, add the Qt-Security header with score
    critical to files where bugs in handling external data are more
    likely to cause security issues.
    
    Only one file here parses external data. The other files
    implement network protocols, but the data they handle is
    already parsed and they are therefore marked significant.
    
    QUIP: 23
    Task-number: QTBUG-135571
    Change-Id: I86b833a166045880a63c290eb5319b208e85c918
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit c4f2dc8af1b682435e6b963a168e345a9b56b9b2)

--- src/httpserver/qhttpserverparser.cpp
+++ src/httpserver/qhttpserverparser.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// Qt-Security score:critical reason:network-protocol
 
 #include "qhttpserverrequest_p.h"
 #include "qhttpserverparser_p.h"
