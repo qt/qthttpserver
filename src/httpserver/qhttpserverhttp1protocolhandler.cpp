36f2163b9945863c60e3d095ada713c434f7becb
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-05-16 19:06:09 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Modified licenseRule.json accordingly.
    
    Task-number: QTBUG-136484
    Change-Id: I602e3711d966a5df9887abfb3bf4967bc09a7333
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/httpserver/qhttpserverhttp1protocolhandler.cpp
+++ src/httpserver/qhttpserverhttp1protocolhandler.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2024 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 // Qt-Security score:significant reason:default
 
 #include "qhttpserverhttp1protocolhandler_p.h"
