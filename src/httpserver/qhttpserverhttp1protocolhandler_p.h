ff51b1f6b582e8f1bf355dff9a696f39808bc223
Øystein Heskestad <oystein.heskestad@qt.io>
2025-06-17 14:52:25 +0200

    Mark remaining files in QtHttpServer as security default
    Removed qhttpserverparser_p.h from cherry-pick because it was
    added in 6.10.
    
    QUIP: 23
    Fixes: QTBUG-135571
    Pick-to: 6.8
    Change-Id: Ic8b3142f50bb536ef50ac6ded645a99ad1f27b46
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
    (cherry picked from commit c16bde0fad9ad712b79ff97b149d173134454c87)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 995173f3198c3978cc942a797094975e544643a5)

--- src/httpserver/qhttpserverhttp1protocolhandler_p.h
+++ src/httpserver/qhttpserverhttp1protocolhandler_p.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// Qt-Security score:significant reason:default
 
 #ifndef QHttpServerHttp1ProtocolHandler_H
 #define QHttpServerHttp1ProtocolHandler_H
