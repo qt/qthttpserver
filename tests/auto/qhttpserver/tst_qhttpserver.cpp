46ee9ede50d6e1cedfe07a5549879748743db44e
Øystein Heskestad <oystein.heskestad@qt.io>
2025-07-09 17:20:29 +0200

    Make tests compile without the concurrency feature enabled
    Add missing include guards and skip tests that need the concurrency
    feature.
    
    Pick-to: 6.10 6.9 6.8
    Change-Id: I8e8c0ef91b8359bf9fb6416f19f70f6c02dc88a9
    Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>

--- tests/auto/qhttpserver/tst_qhttpserver.cpp
+++ tests/auto/qhttpserver/tst_qhttpserver.cpp
@@ -8,7 +8,9 @@
 #include <QtTest/qtest.h>
 #include <QtTest/qsignalspy.h>
 
+#if QT_CONFIG(concurrent)
 #include <QtConcurrent/qtconcurrentrun.h>
+#endif
 
 #include <QtCore/qurl.h>
 #include <QtCore/qstring.h>
@@ -1446,10 +1448,10 @@ void tst_QHttpServer::missingHandler()
     QCOMPARE(sslUsed.toBool(), useSsl);
 }
 
-#if QT_CONFIG(concurrent)
 // Test that responses to pipelined requests come in correct order, see also: QTBUG-105202
 void tst_QHttpServer::pipelinedFutureRequests()
 {
+#if QT_CONFIG(concurrent)
     QFETCH_GLOBAL(bool, useSsl);
     QFETCH_GLOBAL(bool, useHttp2);
     QString urlBase = useSsl ? sslUrlBase : clearUrlBase;
@@ -1469,10 +1471,14 @@ void tst_QHttpServer::pipelinedFutureRequests()
 
     for (std::size_t i = 0; i < replies.size(); i++)
         checkReply(replies[i], QString::number(i));
+#else
+    QSKIP("QtConcurrent is not available, skipping test");
+#endif // QT_CONFIG(concurrent)
 }
 
 void tst_QHttpServer::requestNotOverwritten()
 {
+#if QT_CONFIG(concurrent)
     readySem.emplace();
     routeSem.emplace();
 
@@ -1534,9 +1540,10 @@ void tst_QHttpServer::requestNotOverwritten()
     QCOMPARE(bodySizes.size(), NumberProcessed);
     QCOMPARE(readySem->available(), 0);
     QCOMPARE(routeSem->available(), 0);
-}
-
+#else
+    QSKIP("QtConcurrent is not available, skipping test");
 #endif // QT_CONFIG(concurrent)
+}
 
 void tst_QHttpServer::multipleResponses()
 {
