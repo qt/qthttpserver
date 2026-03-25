f225b5c7769103c20a81a7f93053917de2fd8a12
Marc Mutz <marc.mutz@qt.io>
2026-03-25 10:36:49 +0100

    QHttpServerConfiguration: port from QPair to std::pair
    They're the same (one is a template alias to the other).
    
    Amends 2d9e98680087c91db35f069e9cdbd31ddc50b5d5 (6.10).
    
    Task-number: QTBUG-115841
    Change-Id: I3ae53da184d9982f7d9672e28e55667ac1d13c19
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 54a526c3847bc12f712bc6e7c2023224e0182d1a)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit ac58413acd704466a34f4278f11aea41d7b97c13)
    Reviewed-by: Øystein Heskestad <oystein.heskestad@qt.io>

--- src/httpserver/qhttpserverconfiguration.cpp
+++ src/httpserver/qhttpserverconfiguration.cpp
@@ -25,8 +25,8 @@ public:
 
     quint32 rateLimit = 0;
     std::chrono::seconds keepAliveTimeout = std::chrono::seconds(15);
-    QList<QPair<QHostAddress, int>> whitelist;
-    QList<QPair<QHostAddress, int>> blacklist;
+    QList<std::pair<QHostAddress, int>> whitelist;
+    QList<std::pair<QHostAddress, int>> blacklist;
 };
 
 QT_DEFINE_QESDP_SPECIALIZATION_DTOR(QHttpServerConfigurationPrivate)
